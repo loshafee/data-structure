c08a8904de988cb7066416bf25a7b3d3df4a733a
loshafee <shafeelo@qq.com>
2018-02-15 18:08:57 +0800

    add Stack application function isPalindrome

--- src/Stack.application.js
+++ src/Stack.application.js
@@ -1,5 +1,11 @@
 const Stack = require('./Stack')
 
+/**
+ * 数制转换
+ * @param {Number} num 十进制数
+ * @param {Number} base 需要转换的进制数
+ * @return {String} - 进制数转换后的结果
+ */
 let mulBase = function (num, base) {
   let s = new Stack()
   do {
@@ -14,6 +20,34 @@ let mulBase = function (num, base) {
   return converted
 }
 
+
+/**
+ * 回文数判断
+ * @param {String} word - 字符串
+ * @return {Boolean} 结果
+ */
+let isPalindrome  = function (word) {
+  let s = new Stack(),
+    i = 0
+
+  while (i < word.length) {
+    s.push(word[i])
+    i++
+  }
+
+  let reverseString = ''
+  while (s.length()) {
+    reverseString += s.pop()
+  }
+
+  if (word === reverseString) {
+    return true
+  }
+
+  return false
+}
+
 module.exports = {
+  isPalindrome,
   mulBase
 }
