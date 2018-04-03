f738f8684777b1d09c2cc21308020b0ed286e2a2
loshafee <shafeelo@qq.com>
2018-04-04 00:05:50 +0800

    Add Set test unit

--- test/Set.test.js
+++ test/Set.test.js
@@ -8,6 +8,31 @@ describe('#Set', function () {
 
       assert.equal(set.dataStore.length, 0)
     })
-    
   })
+
+  
+  describe('#方法', function () {
+    it('#add', function () {
+      let set = new Set()
+      set.add('David')
+      set.add('Jennifer')
+
+      assert.equal(set.dataStore.length, 2)
+      assert.equal(set.dataStore[0], 'David')
+      assert.equal(set.dataStore[1], 'Jennifer')
+    })
+
+    it('#remove', function () {
+      let set = new Set()
+      set.add('David')
+      set.add('Jennifer')
+
+      assert.equal(true, set.remove('David'))
+      assert.equal(set.dataStore.length, 1)
+      assert.equal(false, set.remove('Tom'))
+      assert.equal(set.dataStore.length, 1)      
+    })
+  })
+
+
 })
