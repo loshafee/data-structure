394961fb0b51a2f64704014b67f73a36de45433b
loshafee <shafeelo@qq.com>
2018-04-14 00:09:46 +0800

    add Graph class

---
+++ src/Graph.js
@@ -0,0 +1,34 @@
+class Graph {
+  constructor (v) {
+    this.vertices = v
+    this.edges = 0
+    this.adj = []
+    for (let i = 0; i < this.vertices; ++i) {
+      this.adj[i] = []
+      this.adj[i].push('')
+    }
+  }
+
+  addEdge (v, w) {
+    this.adj[v].push(w)
+    this.adj[w].push(v)
+    this.edges++
+  }
+
+  showGraph () {
+    for (let i = 0; i < this.vertices; ++i) {
+      for (let j = 0; j < this.vertices; ++j) {
+        if (this.adj[i][j] !== undefined) {
+          console.log(this.adj[i][j])
+        }
+      }
+      
+    }
+  }
+
+  toString () {
+
+  }
+}
+
+module.exports = Graph
